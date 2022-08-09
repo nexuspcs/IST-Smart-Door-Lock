6e144bef77556410ad7751208fd150ceeb19edf6
npcs <69493073+nexuspcs@users.noreply.github.com>
2022-08-09 20:02:27 +1000

    Update testRGB-led.py

--- other(not-needed)/testRGB-led.py
+++ other(not-needed)/testRGB-led.py
@@ -1,5 +1,37 @@
 # Testing rgb led on Pi
 
-import RPi.GPIO as GPIO # Library for servo motor control
-from time import sleep # Delays 
+# External module imports
+import RPi.GPIO as GPIO
 import time
+
+# Pin Definitons:
+ledPin = 23 # Broadcom pin 23 (P1 pin 16)
+
+
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
+pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
+GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+
+# Initial state for LEDs:
+GPIO.output(ledPin, GPIO.LOW)
+pwm.start(dc)
+
+print("Here we go! Press CTRL+C to exit")
+try:
+    while 1:
+        if GPIO.input(butPin): # button is released
+            pwm.ChangeDutyCycle(dc)
+            GPIO.output(ledPin, GPIO.LOW)
+        else: # button is pressed:
+            pwm.ChangeDutyCycle(100-dc)
+            GPIO.output(ledPin, GPIO.HIGH)
+            time.sleep(0.075)
+            GPIO.output(ledPin, GPIO.LOW)
+            time.sleep(0.075)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    pwm.stop() # stop PWM
+    GPIO.cleanup() # cleanup all GPIO
