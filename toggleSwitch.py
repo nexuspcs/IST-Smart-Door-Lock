c83ce0ac32675e635a5bdadf160d064fc70b310f
npcs <69493073+nexuspcs@users.noreply.github.com>
2022-08-10 20:42:46 +1000

    Add code into github for switch

---
+++ toggleSwitch.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+import time 
+
+from time import sleep 
+
+buttonInput = 29
+returnGPIOpin = 31
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(buttonInput, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(returnGPIOpin, GPIO.OUT)
+
+GPIO.output(returnGPIOpin, GPIO.LOW)
+
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+pwm=GPIO.PWM(8, 50)
+pwm.start(0)
+
+
+
+
+ledPinRed = 11
+GPIO.setup(ledPinRed, GPIO.OUT)
+ledPinGreen = 23
+GPIO.setup(ledPinGreen, GPIO.OUT)
+
+GPIO.output(ledPinRed, GPIO.HIGH)
+GPIO.output(ledPinGreen, GPIO.HIGH)
+
+
+
+# initilasing the servo motor
+GPIO.output(13, GPIO.HIGH)
+
+
+
+
+
+while True:
+        if GPIO.input(buttonInput) == GPIO.HIGH:
+                print("GPIO is high, so will unlock")
+                time.sleep(0.2)
+        if GPIO.input(buttonInput) == GPIO.LOW:
+                print("GPIO is low, so will lock")
+                # push button down = unlock
+                GPIO.output(ledPinGreen, GPIO.LOW)
+                GPIO.output(ledPinRed, GPIO.HIGH)
+                                
+                # some simple math for Pulse width Modulation 
+                def SetAngle(angle):
+                        duty = angle / 18 + 2
+                        GPIO.output(8, True)
+                        pwm.ChangeDutyCycle(duty)
+                        sleep(1)
+                        GPIO.output(8, False)
+                        pwm.ChangeDutyCycle(0)
+                SetAngle(90)
+
+
+
+
+                
+        else:
+                print("no switch position, dormant")
