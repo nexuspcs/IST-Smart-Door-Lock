5685aca534d964efc904596e81254709ac284a95
npcs <69493073+nexuspcs@users.noreply.github.com>
2022-08-10 20:45:59 +1000

    Update toggleSwitch.py

--- toggleSwitch.py
+++ toggleSwitch.py
@@ -21,6 +21,7 @@ GPIO.output(returnGPIOpin, GPIO.LOW)
 #Initialising the servo motor
 GPIO.setup(8, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
+GPIO.output(13, GPIO.HIGH)
 pwm=GPIO.PWM(8, 50)
 pwm.start(0)
 
@@ -37,19 +38,18 @@ GPIO.output(ledPinGreen, GPIO.HIGH) # setting the gpio pin to high (a.k.a will t
 
 
 
-# initilasing the servo motor
-GPIO.output(13, GPIO.HIGH)
 
 
 
 
 
-while True:
-        if GPIO.input(buttonInput) == GPIO.HIGH:
+
+while True: # infinite loop
+        if GPIO.input(buttonInput) == GPIO.HIGH: # push switch up = lock 
                 print("GPIO is high, so will unlock")
                 time.sleep(0.2)
-        if GPIO.input(buttonInput) == GPIO.LOW:
-                print("GPIO is low, so will lock")
+        if GPIO.input(buttonInput) == GPIO.LOW: # push switch down = unlock
+                print("GPIO is low, so will unlock")
                 # push button down = unlock
                 GPIO.output(ledPinGreen, GPIO.LOW)
                 GPIO.output(ledPinRed, GPIO.HIGH)
